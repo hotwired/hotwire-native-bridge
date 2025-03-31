8fc7ace93c4c5ecfb8eac709dbee363979f29d7c
Fernando Olivares <fernando.olivares@me.com>
2025-03-31 13:33:26 -0600

    Use HotwireNative window object

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import { Bridge } from "./bridge"
 export { BridgeComponent } from "./bridge_component"
 export { BridgeElement } from "./bridge_element"
 
-if (!window.Strada) {
+if (!window.HotwireNative) {
   const webBridge = new Bridge()
+  window.HotwireNative = { web: webBridge }
+
+  // Legacy client support
   window.Strada = { web: webBridge }
+  window.webBridge = webBridge
+  
   webBridge.start()
 }
