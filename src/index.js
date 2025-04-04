4693ccb7fdd8d9932322ac40035440568fff673e
Jay Ohms <jay@37signals.com>
2025-04-04 15:21:14 -0400

    Add better legacy support for alpha Strada clients

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ if (!window.HotwireNative) {
   const webBridge = new Bridge()
   window.HotwireNative = { web: webBridge }
 
-  // Legacy client support
-  window.Strada = { web: webBridge }
-  window.webBridge = webBridge
+  addLegacyClientSupport(webBridge)
   
   webBridge.start()
+}
+
+function addLegacyClientSupport(webBridge) {
+  if (!window.Strada) {
+    window.Strada = { web: webBridge }
+  }
+
+  if (!window.webBridge) {
+    window.webBridge = webBridge
+  }
 }
